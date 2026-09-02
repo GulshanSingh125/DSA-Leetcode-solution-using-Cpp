a06d3b1f9be14d98c35e0a05d19c8dac0fbe953d
Gulshan Singh <168180140+GulshanSingh125@users.noreply.github.com>
2026-09-02 07:27:04 +0530

    Time: 0 ms (100%), Space: 14 MB (93.63%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,46 +1,27 @@
 class Solution {
 public:
-
-    void backtrack(vector<int>& candidates,
-                   int target,
-                   int start,
-                   vector<int>& curr,
-                   vector<vector<int>>& ans) {
-
-        if(target == 0) {
-            ans.push_back(curr);
+    void backTrack(vector<int> &candidates, int target, int start,vector<int> &current, vector<vector<int>> &ans){
+        if(target == 0){
+            ans.push_back(current);
+            return;
+        }
+        if(target<0){
             return;
         }
 
-        if(target < 0) {
-            return;
+        for(int i=start;i<candidates.size();i++){
+            current.push_back(candidates[i]);
+            backTrack(candidates,target-candidates[i],i,current,ans);
+            current.pop_back();
         }
 
-        for(int i = start; i < candidates.size(); i++) {
-
-            // Choose
-            curr.push_back(candidates[i]);
-
-            // Explore
-            backtrack(candidates,
-                      target - candidates[i],
-                      i,
-                      curr,
-                      ans);
-
-            // Undo
-            curr.pop_back();
-        }
     }
 
 
     vector<vector<int>> combinationSum(vector<int>& candidates,int target) {
-
-        vector<int> curr;
+        vector<int> current;
         vector<vector<int>> ans;
-
-        backtrack(candidates, target, 0, curr, ans);
-
+        backTrack(candidates,target,0,current,ans);
         return ans;
     }
 };
